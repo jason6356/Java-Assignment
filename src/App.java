d5156775ff5f25ed75c6950d40c202708a261052
jason6356 <70106185+jason6356@users.noreply.github.com>
2021-07-13 17:39:26 +0800

    removed the errors of the file

--- src/App.java
+++ src/App.java
@@ -12,39 +12,27 @@ public class App {
         System.out.println(a + b);
 
 
-        123456
+        //123456
 
-        abcdefg
-
-
-
-    1. ctrl + shift + p
-    2. type Git: clone 
-    3. paste the url below 
-    https://github.com/jason6356/Java-Assignment 
-        done import the assignment file :) 
-
-    Before you start coding (to synchronize the latest code from the others)
-    1. bottom left got a word "Master", beside it got an icon looks like refresh 
-        - press it and press ok 
-
-    After you done coding (to synchronize the latest code to github )
-    1. press the source control on your right hand side ( third icon [below search] )
-    2. enter related message (ep: added 1 new method) and press ctrl+enter, then press ok 
-    3. bottom left got a word "Master", beside it got arrow up and down ( same position as the refresh button )
-        - press it and press ok
-    4. done
+        //abcdefg
 
 
+    // 1. ctrl + shift + p
+    // 2. type Git: clone 
+    // 3. paste the url below 
+    // https://github.com/jason6356/Java-Assignment 
+    //     done import the assignment file :) 
 
+    // Before you start coding (to synchronize the latest code from the others)
+    // 1. bottom left got a word "Master", beside it got an icon looks like refresh 
+    //     - press it and press ok 
 
-
-
-
-
-
-
-
+    // After you done coding (to synchronize the latest code to github )
+    // 1. press the source control on your right hand side ( third icon [below search] )
+    // 2. enter related message (ep: added 1 new method) and press ctrl+enter, then press ok 
+    // 3. bottom left got a word "Master", beside it got arrow up and down ( same position as the refresh button )
+    //     - press it and press ok
+    // 4. done
 
     
     }
