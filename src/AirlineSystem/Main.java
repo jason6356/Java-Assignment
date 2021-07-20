701fcd8b860b96ed127bcdd3c75d59734237d0f1
victorwong02 <wongjw-wm20@student.tarc.edu.my>
2021-07-20 22:28:40 +0800

    u gay

--- src/AirlineSystem/Main.java
+++ src/AirlineSystem/Main.java
@@ -21,6 +21,6 @@ public class Main {
     // 4. done
 
     System.out.println("Hello world");    
-    System.out.println("KANGSHENG VERY GAYYYY");
+    System.out.println("KANGSHENG SIBEH LAN 7 GAYYYY");
 }
 }
