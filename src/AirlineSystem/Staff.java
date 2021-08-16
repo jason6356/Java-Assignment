a7df9a1188863b9e36474788691fe72b5436fd24
nlsh411 <nicolelai031104@gmail.com>
2021-08-16 21:38:44 +0800

    Staff Class

---
+++ src/AirlineSystem/Staff.java
@@ -0,0 +1,31 @@
+package AirlineSystem;
+
+public class Staff {
+    private String staffID;
+    private String password; 
+
+    //method 
+    public boolean addFilght(Flight flight){
+
+    }
+
+    public boolean addAirport(Airport airport){
+
+    }
+
+    public boolean addAirline(Airline airline){
+
+    }
+
+    public void checkRequest(){
+
+    }
+
+    public void acceptRequest(){
+
+    }
+
+    public void rejectRequest(){
+        
+    }
+}
